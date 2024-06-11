4edd3f0161c51852c49f1e6869e54340f8f199d6
dinesh <dineshsutharxii@gmail.com>
2024-06-11 19:52:17 +0530

    lambda function Practice

---
+++ Day_four/day_four_practice_python.py
@@ -0,0 +1,20 @@
+# lambda function
+# - A lambda function is a small anonymous function.
+# - A lambda function can take any number of arguments, but can only have one expression
+
+sumi = lambda x: x * 2
+print(sumi(2))
+
+print((lambda x, y, z: x + y + z)(2, 3, 1))
+
+full_name = lambda first, last: f"Full Name: {first} and Last Name: {last}"
+print(full_name('Dipak', 'Singh'))
+
+
+def myfunc(n):
+    return lambda a: a * n
+
+
+mydoubler = myfunc(2)
+
+print(mydoubler(11))
