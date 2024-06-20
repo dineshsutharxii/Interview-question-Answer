5494d08b47cc4cd8332bcbb39cf74b392d147719
dinesh <dineshsutharxii@gmail.com>
2024-06-20 10:39:29 +0530

    Added explicit wait and new methods in FlightPage.py

--- Project/Pages/FlightPage.py
+++ Project/Pages/FlightPage.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from Project.Pages.BasePage import BasePage
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class FlightPage(BasePage):
@@ -8,13 +10,18 @@ class FlightPage(BasePage):
         self.driver = driver
 
     # locator
-    SEARCH_BUTTON_xpath = "//*[@id='top-banner']//a[contains(text(),'Search')]"
-    from_city_xpath = "//label[@for='fromCity']"
-    from_city_enter_text_xpath = "//input[@id='fromCity']"
-    from_city_suggestions_xpath = "//ul[@role='listbox']/li"
+    SEARCH_BUTTON = (By.XPATH, "//*[@id='top-banner']//a[contains(text(),'Search')]")
+    from_city_xpath = (By.XPATH, "//label[@for='fromCity']")
+    from_city_enter_text_xpath = (By.XPATH, "//input[@id='fromCity']")
+    from_city_suggestions_xpath = (By.XPATH, "//ul[@role='listbox']/li")
 
     def Click_Search(self):
-        self.driver.find_element(By.XPATH, self.SEARCH_BUTTON_xpath).click()
+        wait = WebDriverWait(self.driver, 10)
+        try:
+            element = wait.until(EC.element_to_be_clickable(self.SEARCH_BUTTON))
+            self.driver.find_element(self.SEARCH_BUTTON).click()
+        except Exception as e:
+            print("The error is : ", e)
 
     def Click_From(self):
         self.driver.find_element(By.XPATH, self.from_city_xpath).click()
@@ -25,4 +32,3 @@ class FlightPage(BasePage):
     def Select_city_from_suggestion_list(self):
         suggestion_list = self.driver.find_elements(By.XPATH, self.from_city_suggestions_xpath)
         suggestion_list[0].click()
-
