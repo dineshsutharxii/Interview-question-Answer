46457d4b4432b3f9c710b27705055a137d4e0387
dinesh <dineshsutharxii@gmail.com>
2024-06-23 15:17:36 +0530

    Added some datetime module practice

--- Day_three/day_three_practice_python.py
+++ Day_three/day_three_practice_python.py
@@ -1,28 +1,39 @@
 # find amx and Min in array
 
-arr1 = [5, 6, 8, 12, 1, 5]
-# 1st way
-print(min(arr1))
-print(max(arr1))
+# arr1 = [5, 6, 8, 12, 1, 5]
+# # 1st way
+# print(min(arr1))
+# print(max(arr1))
 
-# 2nd way
-maxi = arr1[0]
-for i in range(1, len(arr1)):
-    if arr1[i] > maxi:
-        maxi = arr1[i]
-print("maxi : ", maxi)
-
-mini = arr1[0]
-for i in range(1, len(arr1)):
-    if arr1[i] < mini:
-        mini = arr1[i]
-print("mini : ", mini)
-
-# find length of array
-length = len(arr1)
-count = 0
-for ele in arr1:
-    count += 1
-print("Size : ", count)
-
+# # 2nd way
+# maxi = arr1[0]
+# for i in range(1, len(arr1)):
+#     if arr1[i] > maxi:
+#         maxi = arr1[i]
+# print("maxi : ", maxi)
 #
+# mini = arr1[0]
+# for i in range(1, len(arr1)):
+#     if arr1[i] < mini:
+#         mini = arr1[i]
+# print("mini : ", mini)
+#
+# # find length of array
+# length = len(arr1)
+# count = 0
+# for ele in arr1:
+#     count += 1
+# print("Size : ", count)
+
+#datetime
+import datetime
+
+current_date = datetime.datetime.today().date()
+current_time = datetime.datetime.today().time()
+current_datetime = datetime.datetime.today()
+print(current_date)
+print(current_time)
+print(current_datetime)
+
+filename = current_datetime.strftime('%Y_%m_%d_%H_%M_%S_%f')
+print(filename)
--- Project/Test/test_loginPage.py
+++ Project/Test/test_loginPage.py
@@ -9,10 +9,10 @@ from Project.Pages.BasePage import BasePage
 @pytest.fixture(scope="class")
 class Test_loginPage:
     # Test Case 2.1: Verify that the "One-way" radio button is selected by default
-    # def test_one_way_selected(self, driver):
-    #     one_way_radio = driver.find_element(By.XPATH, "//*[@id='root']/div[3]/div/div/div[1]/ul/li[1]/span")
-    #     assert one_way_radio.is_selected(), "One-way radio button is not selected by default"
-    #     print("One-way radio button is selected by default")
+    def test_one_way_selected(self, driver):
+        one_way_radio = driver.find_element(By.XPATH, "//*[@id='root']/div[3]/div/div/div[1]/ul/li[1]/span")
+        assert one_way_radio.is_selected(), "One-way radio button is not selected by default"
+        print("One-way radio button is selected by default")
 
     def test_search_one_way_tickets(self):
         flightpage.Click_From()
