dda4daaba9e8830838fa0841918d10950836c44d
dinesh <dineshsutharxii@gmail.com>
2024-06-28 19:13:41 +0530

    added some new interview questions

--- Project/testcases/conftest.py
+++ Project/testcases/conftest.py
@@ -1,20 +1,24 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.chrome.options import Options
+# from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="class")
 def setup(request):
-    chrome_options = Options()
-    # chrome_options.add_argument("--disable-extensions")
-    # chrome_options.add_argument("--disable-extensions")
-    # chrome_options.add_argument("--enable-automation")
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_experimental_option("useAutomationExtension", False)
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+    service_obj = Service(ChromeDriverManager().install())
+    driver = webdriver.Chrome(options=chrome_options, service=service_obj)
+
+    # chrome_options.add_argument("--disable-automation")
     # chrome_options.add_argument('start-maximized')
     # chrome_options.add_argument('disable-infobars')
-    driver = webdriver.Chrome(options=chrome_options, executable_path=ChromeDriverManager().install())
+    # driver = webdriver.Chrome(options=chrome_options, executable_path=ChromeDriverManager().install())
     wait = WebDriverWait(driver, 10)
-    driver.get("https://www.makemytrip.com/")
+    driver.get("https://www.yatra.com/")
     driver.maximize_window()
     request.cls.driver = driver  # this will make "driver" refernce availble at class level. - cls means class
     request.cls.wait = wait # this will make "wait" refernce availble at class level.
