c38d88a7d0ac0b7ff14bfe097f7dd72607c4c454
dinesh <dineshsutharxii@gmail.com>
2024-07-03 18:58:38 +0530

    changes in basepage

--- Project/base/BasePage.py
+++ Project/base/BasePage.py
@@ -7,13 +7,13 @@ class BasePage:
 
     def page_scroll(self):
         pageLength = self.driver.execute_script(
-            "window.scrollTo(0, document.body.scrollHeight); var pageLength=document.body.scrollHeight;")
+            "window.scrollTo(0, document.body.scrollHeight);var pageLength=document.body.scrollHeight;return pageLength;")
         match = False
         while match == False:
             lastCount = pageLength
-            time.sleep(2)
+            time.sleep(1)
             pageLength = self.driver.execute_script(
-                "window.scrollTo(0, document.body.scrollHeight); var pageLength=document.body.scrollHeight;")
+                "window.scrollTo(0, document.body.scrollHeight);var pageLength=document.body.scrollHeight;return pageLength;")
             if lastCount == pageLength:
                 match = True
-        time.sleep(2)
+        time.sleep(4)
