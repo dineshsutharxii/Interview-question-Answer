9f8bb14f81cbdc2572b97ce3840263c8110b9297
dinesh <dineshsutharxii@gmail.com>
2024-07-10 16:04:45 +0530

    Implementing logging in framework

--- Project/utilities/logging_functionality.py
+++ Project/utilities/logging_functionality.py
@@ -1,17 +1,22 @@
 import logging
 
+
 class Logging:
-    def logger(self):
-        #create logger
+    def logging_log(self):
+        # create logger
         logger = logging.getLogger(__name__)
         logger.setLevel(logging.DEBUG)
         # Create handler (Console or file handler)
         console_handler = logging.StreamHandler()
-        file_handler = logging.FileHandler('..\Project\Logs\logs.log')
+        file_handler = logging.FileHandler('..\Logs\logs.log')
         # formater
-        console_formatter = logging.Formatter('%(filename)s - %(asctime)s - %(message)s', datefmt='%Y-%m-%d %H:%M:%S,uuu')
+        console_formatter = logging.Formatter('%(filename)s - %(asctime)s - %(message)s',
+                                              datefmt='%Y-%m-%d %H:%M:%S,uuu')
         file_formatter = logging.Formatter('%(filename)s : %(asctime)s : %(message)s')
-        #add handler to logger
+        #setFormatter
+        console_handler.setFormatter(console_formatter)
+        file_handler.setFormatter(file_formatter)
+        # add handler to logger
         logger.addHandler(console_handler)
         logger.addHandler(file_handler)
         #
@@ -20,3 +25,6 @@ class Logging:
         logging.warning('Warning : And this, too')
         logging.error('Error : And non-ASCII stuff, too, like Øresund and Malmö')
 
+
+test_log = Logging()
+test_log.logging_log()
