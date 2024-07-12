6eb8f844febbf3d3b7749bf15ad6f356305767be
dinesh <dineshsutharxii@gmail.com>
2024-07-12 10:58:29 +0530

    Utility for logging debug,Info, warning, error and critical

--- Project/utilities/logging_functionality.py
+++ Project/utilities/logging_functionality.py
@@ -9,12 +9,12 @@ class Logging:
         self.file_handler = None
         self.console_handler = None
 
-    def logs(self):
-        self.logger = logging.getLogger("demologs")
+    def custom_logger(self):
+        self.logger = logging.getLogger(__name__)
         self.logger.setLevel(logging.DEBUG)
         self.console_handler = logging.StreamHandler()  # Handler for console
-        self.console_handler.setLevel(logging.CRITICAL)  # setLevel to handler
-        self.file_handler = logging.FileHandler('..\Logs\logs.txt')  # Handler for file
+        self.console_handler.setLevel(logging.DEBUG)  # setLevel to handler
+        self.file_handler = logging.FileHandler(r'C:\Users\dines\Learn And Interview\Interview_question_answer\Project\Logs\logs.txt')  # Handler for file
         self.file_handler.setLevel(logging.DEBUG)  # setLevel to handler
         self.console_formatter = logging.Formatter(fmt='%(filename)s - %(asctime)s - %(message)s',
                                                    datefmt='%d-%m-%Y %H:%M:%S')
@@ -24,13 +24,4 @@ class Logging:
         self.file_handler.setFormatter(self.file_formatter)  # add Formatter to handler
         self.logger.addHandler(self.console_handler)  # added handler to logger
         self.logger.addHandler(self.file_handler)  # added handler to logger
-
-        self.logger.debug('Debug level - This message should go to the log file')
-        self.logger.info('Info : So should this')
-        self.logger.warning('Warning : And this, too')
-        self.logger.error('Error : And non-ASCII stuff, too, like Øresund and Malmö')
-        self.logger.critical("Critical: its critical error")
-
-
-test_log = Logging()
-test_log.logs()
+        return self.logger
