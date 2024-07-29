b24ca08cddb9a91d7b1f20741751a0681bcf4449
dinesh <dineshsutharxii@gmail.com>
2024-07-29 19:03:26 +0530

    Edited the confest

--- Project_new/testcases/conftest.py
+++ Project_new/testcases/conftest.py
@@ -15,13 +15,13 @@ def setup(request):
     chrome_options.add_experimental_option("prefs", prefs)
     chrome_options.add_experimental_option("useAutomationExtension", False)
     # chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    # service_obj = Service(ChromeDriverManager().install())
-    service_obj = Service(r'C:\Users\dines\.wdm\drivers\chromedriver\win64\127.0.6533.72\chromedriver-win32\chromedriver.exe')
+    service_obj = Service(ChromeDriverManager().install())
+    service_obj = Service(r'C:\Users\dines\.wdm\drivers\chromedriver\win64\driver\chromedriver-win32\chromedriver.exe')
 
     driver = webdriver.Chrome(options=chrome_options, service=service_obj)
     wait = WebDriverWait(driver, 10)
-    # driver.get("https://www.yatra.com/")
-    driver.get("https://demo.softneta.com/search.html")
+    driver.get("https://www.yatra.com/")
+    # driver.get("https://demo.softneta.com/search.html")
     driver.maximize_window()
     request.cls.driver = driver  # this will make "driver" reference available at class level. - cls means class
     request.cls.wait = wait  # this will make "wait" reference available at class level.
